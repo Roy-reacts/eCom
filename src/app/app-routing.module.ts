414491d33a459dac1a8de068bd75bb49893d7f26
Roy-reacts <snehasis_suvro@yahoo.com>
2022-10-16 15:24:13 +0600

    [commit message]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/Home/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartComponent } from './components/Product/cart/cart.component';
+import { CheckoutComponent } from './components/Product/checkout/checkout.component';
+import { PreorderComponent } from './components/Product/preorder/preorder.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'product-details', component:ProductDetailsComponent},
+  {path:'cart', component:CartComponent},
+  {path:'checkout', component:CheckoutComponent},
+  {path:'pre-order', component:PreorderComponent},
+  {path:'**', component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
